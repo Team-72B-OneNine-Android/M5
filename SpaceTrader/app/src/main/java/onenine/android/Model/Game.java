52dcca1e2dc6559568c56ef7e61d05e17ae0703e
Gauranshu Sharma <gsharma@lawn-143-215-90-189.lawn.gatech.edu>
2019-02-17 13:23:22 -0500

    created Facade class and edited Game class

--- SpaceTrader/app/src/main/java/onenine/android/Model/Game.java
+++ SpaceTrader/app/src/main/java/onenine/android/Model/Game.java
@@ -2,4 +2,13 @@ package onenine.android.Model;
 
 public class Game {
 
+    private GameDifficulty difficulty;
+
+    public GameDifficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+//    public void setDifficulty(GameDifficulty diff) {
+//        this.difficulty = diff;
+//    }
 }
---
+++ SpaceTrader/app/src/main/java/onenine/android/ViewModel/Facade.java
@@ -0,0 +1,8 @@
+package onenine.android.ViewModel;
+
+import onenine.android.Model.Game;
+
+public class Facade {
+
+    private Game game;
+}
