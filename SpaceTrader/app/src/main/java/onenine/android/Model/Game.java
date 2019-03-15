35527352bdd201d6983fc0dfd69883287a8506df
garrisonramey <garrisonramey@gmail.com>
2019-03-15 07:37:18 -0400

    There is a bug with our travel method

--- SpaceTrader/app/src/main/java/onenine/android/Model/Game.java
+++ SpaceTrader/app/src/main/java/onenine/android/Model/Game.java
@@ -44,6 +44,7 @@ public class Game implements Serializable {
     }
 
     public boolean travel(Planet p) {
+        /*
         if (this.fuelCostForPlanet(p) != 0) {
             randomEvent = Events.checkForEvent();
             if (randomEvent == Events.LOSE_CARGO) {
@@ -68,6 +69,31 @@ public class Game implements Serializable {
         } else {
             return false;
         }
+        */
+        if (player.getShipFuel() >= fuelCostForPlanet(p)) {
+            randomEvent = Events.checkForEvent();
+            this.setCurrentPlanet(p);
+            int distance = currentPlanet.calculateDistance(p);
+            player.updateShipFuel(distance);
+            if (randomEvent == Events.LOSE_CARGO) {
+                player.lossOfCargo();
+                return true;
+            }
+            if (randomEvent == Events.LOSE_CREDIT) {
+                if (!(player.getCredits() <= 0)) {
+                    player.changeCredits(-100);
+                }
+                return true;
+            }
+            if (randomEvent == Events.GAIN_CREDIT) {
+                player.changeCredits(100);
+                return true;
+            }
+        } else {
+            randomEvent = Events.NO_EVENT;
+            return true;
+        }
+        return false;
     }
 
     public int fuelCostForPlanet(Planet planet) {
