3f5c3ca3f27103bc08656a64415446ba9dc72933
garrisonramey <garrisonramey@gmail.com>
2019-03-26 15:47:31 -0400

    Finished ShipType Javadocs

--- SpaceTrader/app/src/main/java/onenine/android/Model/ShipType.java
+++ SpaceTrader/app/src/main/java/onenine/android/Model/ShipType.java
@@ -6,10 +6,20 @@ public enum ShipType {
 
     private final int cargoSpace;
 
+    /**
+     * Ship type constructor that sets the amount of cargo space
+     *
+     * @param cargoSpace the amount of cargo space for a given shiptype
+     */
     ShipType(int cargoSpace) {
         this.cargoSpace = cargoSpace;
     }
 
+    /**
+     * Gets amount of cargo space a ship has
+     *
+     * @return the amount of cargo space for a ship
+     */
     public int getCargoSpace() {
         return cargoSpace;
     }
