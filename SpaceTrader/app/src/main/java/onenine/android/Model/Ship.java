0f40b7c4aaf1dca0b9d5308c98ee1a979214ac8d
garrisonramey <garrisonramey@gmail.com>
2019-03-26 15:44:32 -0400

    Finished Ship Javadocs

--- SpaceTrader/app/src/main/java/onenine/android/Model/Ship.java
+++ SpaceTrader/app/src/main/java/onenine/android/Model/Ship.java
@@ -9,6 +9,11 @@ public class Ship implements Serializable {
     private int shipFuel;
     private HashMap<Goods, Integer> tradeGoods = new HashMap<>();
 
+    /**
+     * Ship constructor
+     *
+     * @param st the ship type to set the ship as
+     */
     public Ship (ShipType st) {
         this.shipType = st;
         this.cargoUsed = 0;
@@ -18,37 +23,83 @@ public class Ship implements Serializable {
         }
     }
 
+    /**
+     * Gets the ship type
+     *
+     * @return the ship type
+     */
     public ShipType getShipType() {
         return shipType;
     }
 
+    /**
+     * Gets the amoutn of cargo used on the ship
+     *
+     * @return the amount of cargo used
+     */
     public int getCargoUsed() {
         return cargoUsed;
     }
 
+    /**
+     * Sets the amount of cargo used on the ship
+     *
+     * @param cargoUsed the amount of cargo to give the ship
+     */
     public void setCargoUsed(int cargoUsed) {
         this.cargoUsed = cargoUsed;
     }
+
+    /**
+     * Gets the amount of fuel for ship
+     *
+     * @return the amount of fuel a ship has
+     */
     public int getShipFuel() {
         return shipFuel;
     }
 
+    /**
+     * Sets amount of fuel for a ship
+     *
+     * @param shipFuel the amount of fuel to give a ship
+     */
     public void setShipFuel(int shipFuel) {
         this.shipFuel = shipFuel;
     }
 
+    /**
+     * Gets the trade goods
+     *
+     * @return trade goos for a ship
+     */
     public HashMap<Goods, Integer> getTradeGoods() {
         return tradeGoods;
     }
 
+    /**
+     * Gets the amount of cargo space remaining for a ship
+     *
+     * @return the amount of cargo space that a ship has
+     */
     public int getCargoSpace() {
         return shipType.getCargoSpace() - cargoUsed;
     }
 
+    /**
+     * Determines whether there is cargo space available for a ship
+     *
+     * @return wheter or not cargo space is available for a ship
+     */
     public boolean cargoSpaceAvailable() {
         return cargoUsed < shipType.getCargoSpace();
     }
 
+    /**
+     * Gets the goods that are currently on the ship
+     *
+     * @return the goods that are currently on the ship
+     */
     public String currentGoods() {
         String currentGoods = "";
         for (Goods goods : tradeGoods.keySet()) {
@@ -65,12 +116,21 @@ public class Ship implements Serializable {
         return currentGoods;
     }
 
+    /**
+     * Decreases fuel for a ship based on a provided distance
+     *
+     * @param distance the distance to travel
+     * @return the amount the fuel should be decreased by
+     */
     public int decreaseFuel(int distance) {
         int percentDecrease = distance / 2;
         shipFuel = shipFuel - percentDecrease;
         return shipFuel;
     }
 
+    /**
+     * Empties the cargo for a ship
+     */
     public void emptyCargo() {
         if (cargoUsed != 0) {
             for (Goods goods : tradeGoods.keySet()) {
