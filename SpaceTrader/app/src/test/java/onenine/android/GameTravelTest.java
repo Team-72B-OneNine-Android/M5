690f8092b53164703f77004048ad3ece4afda688
Nipun Goyal <nipun91@yahoo.com>
2019-04-01 13:03:50 -0400

    JUnit started

---
+++ SpaceTrader/app/src/test/java/onenine/android/GameTravelTest.java
@@ -0,0 +1,15 @@
+package onenine.android;
+
+import org.junit.*;
+
+import onenine.android.Model.Game;
+import onenine.android.Model.Universe;
+
+import static org.junit.Assert.*;
+
+public class GameTravelTest {
+
+    private Game game;
+    private Universe universe;
+    
+}
