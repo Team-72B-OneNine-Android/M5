71ba1e864fd79ee75ec1c4bf8c0ae04b1112bd3d
garrisonramey <garrisonramey@gmail.com>
2019-04-02 16:09:09 -0400

    Javadocs for GoodsViewModel

--- SpaceTrader/app/src/main/java/onenine/android/ViewModel/GoodsViewModel.java
+++ SpaceTrader/app/src/main/java/onenine/android/ViewModel/GoodsViewModel.java
@@ -9,12 +9,27 @@ public class GoodsViewModel extends ViewModel {
 
     private final Facade gameFacade = Facade.getInstance();
 
+    /**
+     * Gets the current game
+     *
+     * @return the current game
+     */
     public Game getGame() {
         return gameFacade.getGame();
     }
 
+    /**
+     * Gets the player's credits
+     *
+     * @return the amount of credits the player has
+     */
     public int getCredits() {return gameFacade.getPlayerCredits();}
 
+    /**
+     * Gets the cargo space available for the player
+     *
+     * @return the amount of cargo space available for the player
+     */
     public int getCargoSpaceAvailable() {
         return gameFacade.cargoSpace();
     }
