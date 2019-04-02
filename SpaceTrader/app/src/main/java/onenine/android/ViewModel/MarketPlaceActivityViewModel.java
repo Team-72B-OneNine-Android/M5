81a16ffc9b9515a54852c6c68ae319d10e06a01a
garrisonramey <garrisonramey@gmail.com>
2019-04-02 16:15:17 -0400

    Javadocs for MarketPlaceActivity

--- SpaceTrader/app/src/main/java/onenine/android/ViewModel/MarketPlaceActivityViewModel.java
+++ SpaceTrader/app/src/main/java/onenine/android/ViewModel/MarketPlaceActivityViewModel.java
@@ -7,14 +7,29 @@ public class MarketPlaceActivityViewModel extends ViewModel{
 
     private final Facade gameFacade = Facade.getInstance();
 
+    /**
+     * Gets the goods that the player has
+     *
+     * @return the goods the player has in his or her ship
+     */
     public String getCurrentGoods() {
         return gameFacade.shipCurrentGoods();
     }
 
+    /**
+     * Gets the goods produced on the planet the player is on
+     *
+     * @return the goods produced on the planet the player is on
+     */
     public String getGoodsProduced() {
         return gameFacade.planetGoodsProduced();
     }
 
+    /**
+     * Gets the goods used on planet the player is on
+     *
+     * @return the goods used on the planet the player is on
+     */
     public String getGoodsUsed() {
         return gameFacade.planetGoodsUsed();
     }
