e24cf5a70be515296249ecdbecfb04a27565409e
garrisonramey <garrisonramey@gmail.com>
2019-04-02 16:06:33 -0400

    Javadocs for configActVM

--- SpaceTrader/app/src/main/java/onenine/android/ViewModel/ConfigurationActivityViewModel.java
+++ SpaceTrader/app/src/main/java/onenine/android/ViewModel/ConfigurationActivityViewModel.java
@@ -7,6 +7,19 @@ import onenine.android.Model.GameDifficulty;
 
 public class ConfigurationActivityViewModel extends ViewModel {
     private final Facade gameFacade = Facade.getInstance();
+
+    /**
+     * Takes information passed from the configuration activity and creates
+     * the player and game
+     *
+     * @param n player name
+     * @param p player pilot points
+     * @param f player fighter points
+     * @param t player trader points
+     * @param e player engineer points
+     * @param credits player credits
+     * @param difficulty game difficulty
+     */
     public void onConfig(String n, int p, int f, int t, int e, int credits, GameDifficulty difficulty) {
         gameFacade.setPlayer(n, p, f, t, e, credits);
         gameFacade.setGame(difficulty);
